60f5d70fcdf8a62afbc14b795627c5ab4de68b1a
Karthy88 <karthy0808@gmail.com>
2023-07-19 12:15:34 +0530

    New Branch

--- src/com/git/GitPractice.java
+++ src/com/git/GitPractice.java
@@ -4,10 +4,16 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		System.out.println("My Name is Karthick");
-		
+
 	}
+
 	public void branch() {
 		System.out.println(" My New Branch Name is Karthy ");
+	}
+
+	public void newBrranch() {
+		System.out.println(" My Branch Karthick ");
+
 	}
 
 }
